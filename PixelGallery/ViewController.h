9fee8bc7868640085687f9a2cd12bcd394a73e5c
liuyuxuan123 <1010134430@qq.com>
2017-04-17 23:30:57 +0800

    Initial Commit

---
+++ PixelGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PixelGallery
+//
+//  Created by 刘宇轩 on 17/04/2017.
+//  Copyright © 2017 liuyuxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PixelGallery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PixelGallery
+//
+//  Created by 刘宇轩 on 17/04/2017.
+//  Copyright © 2017 liuyuxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
